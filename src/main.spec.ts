ccc6d9ff2eb784f1e93ed11f2975a4c489ebc7a8
cocm1324 <cocm1324@gmail.com>
2022-11-30 20:03:40 -0800

    setting up going on

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,5 +1,50 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { hello } from './main';
 
-test('Test if it runs without error', () => {
-    expect(hello()).toBeUndefined()
+const mockEvent: APIGatewayProxyEvent = {
+    headers: {},
+    httpMethod: 'GET',
+    isBase64Encoded: false,
+    path: '/',
+    pathParameters: {},
+    multiValueHeaders: {},
+    queryStringParameters: {},
+    multiValueQueryStringParameters: {},
+    stageVariables: null,
+    requestContext: {
+        accountId: '',
+        apiId: '',
+        authorizer: null,
+        protocol: 'http',
+        httpMethod: 'GET',
+        identity: {
+            accessKey: '',
+            accountId: '',
+            apiKey: '',
+            apiKeyId: '',
+            caller: null,
+            clientCert: null,
+            cognitoAuthenticationProvider: null,
+            cognitoAuthenticationType: null,
+            cognitoIdentityId: null,
+            cognitoIdentityPoolId: null,
+            principalOrgId: null,
+            sourceIp: '',
+            user: null,
+            userAgent: null,
+            userArn: null
+        },
+        path: '/',
+        stage: '',
+        requestId: '',
+        requestTimeEpoch: 0,
+        resourceId: '',
+        resourcePath: '' 
+    },
+    resource: '',
+    body: ''
+}
+
+test('Test if it runs without error', async () => {
+    expect(hello(mockEvent)).toBeDefined();
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-export const hello = () => {
-    console.log("Hello there");
-}
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-hello();
+export const hello = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
+    console.log(event);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+                input: event,
+            },
+        )
+    };
+}
